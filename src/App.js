2f81ebdeed7c51f5374f7774191bfb91e932c95f
Dries Nicolay <dries@MacBook-Pro-van-Dries.local>
2017-04-21 10:32:16 +0200

    Add google map.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+/*global google*/
 import React, { Component } from 'react';
 import './App.css';
 import AppBar from 'material-ui/AppBar';
@@ -6,10 +7,31 @@ import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 
 class App extends Component {
+
+    componentDidMount () {
+        this.map = this.createMap();
+    }
+
+    createMap () {
+        var antwerp = { lat: 51.2167, lng: 4.4167 };
+        const mapOptions = {
+            zoom: 10,
+            center: antwerp
+        };
+        return new google.maps.Map(this.refs.mapCanvas, mapOptions);
+    }
+
     render () {
+        const mapStyle = {
+            width: '100%',
+            height: '80vh'
+        };
         return (
             <MuiThemeProvider muiTheme={getMuiTheme(darkBaseTheme)}>
-                <AppBar title="App menu" />
+                <div>
+                    <AppBar title="Kaart" />
+                    <div ref="mapCanvas" style={mapStyle} />
+                </div>
             </MuiThemeProvider>
         );
     }
